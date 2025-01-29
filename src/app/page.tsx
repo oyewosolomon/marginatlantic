94a54547c931b5dae3d88f0ddad88edd9460f456
oyewosolomon <oyewosolomon@gmail.com>
2025-01-29 11:38:29 +0100

    New

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import CTASection from "./components/CTASection";
 import Features from "./components/Features";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
-import Integrations from "./components/Integrations";
 import Testimonials from "./components/Testimonials";
 import WhyChooseUs from "./components/WhyChooseUs";
 
@@ -13,7 +12,7 @@ export default function Home() {
    <Features/>
    <WhyChooseUs/>
    <Testimonials/>
-   {/* <Integrations/> */}
+
    <CTASection/>
    <Footer/>
    </>
